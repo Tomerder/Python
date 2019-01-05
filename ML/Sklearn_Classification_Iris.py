779d535dca3098454dfb253be01b621686fc822b
Tomerder <tomerder@gmail.com>
2019-01-05 21:18:19 +0200

    Add accuracy calculation

--- ML/Sklearn_Classification_Iris.py
+++ ML/Sklearn_Classification_Iris.py
@@ -2,34 +2,71 @@ from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
+from sklearn.model_selection import train_test_split
 from enum import Enum    
 
-# ---------------------- Enums ---------------------
+#---------------------- Enums ---------------------
+
 class Model(Enum):
     KNN = 1
     LOG_REG = 2
-# --------------------------------------------------
+
+#---------------------- Consts ---------------------
+
+TEST_SIZE = 0.2
+
+#--------------------- Inner imp ----------------------
+
+def InitModel(_model):
+    if _model==Model.KNN:
+        model = KNeighborsClassifier()
+    elif _model==Model.LOG_REG:
+        model = LogisticRegression(solver='lbfgs', multi_class='auto', max_iter=11111)
+    
+    return model
+
+#--------------------- APIs ------------------------
 
 def Prediction(_dataSet, _toPredict, _useModel):
     X = _dataSet.data
     y = _dataSet.target
 
-    if _useModel==Model.KNN:
-        model = KNeighborsClassifier()
-    elif _useModel==Model.LOG_REG:
-        model = LogisticRegression(solver='lbfgs', multi_class='auto', max_iter=11111)
+    model = InitModel(_useModel)
 
     #fit (train) 
     model.fit(X,y)
 
-    #predict with KNN and logReg
+    #predict
     prediction = model.predict(_toPredict)
 
     #return prediction
     return prediction
 
-# ---------------------- main ----------------------
-print('Here we GO!')	
+#--------------------------------------------------
+
+def PredictionAccuracy(_dataSet, _useModel, _testSize):
+    X = _dataSet.data
+    y = _dataSet.target
+    #split DS
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = _testSize)
+
+    model = InitModel(_useModel)
+
+    #fit (train) 
+    model.fit(X_train,y_train)
+
+    #predict 
+    y_prediction = model.predict(X_test)
+
+    #calc accuracy
+    accuracy = metrics.accuracy_score(y_test, y_prediction)
+
+    #return prediction
+    return accuracy
+
+#---------------------- main ----------------------
+
+print('Here we GO!\n')	
 
 #load dataset
 iris = load_iris()
@@ -39,8 +76,18 @@ toPredict = [[2,4,3,1]]
 predictionKnn = Prediction(iris, toPredict, Model.KNN)
 predictionLogReg = Prediction(iris, toPredict, Model.LOG_REG)
 
-#print results
-print(iris.target_names[predictionKnn])
-print(iris.target_names[predictionLogReg])
+#print prediction results
+print("Results for prediction :")
+print("Knn = " + str(iris.target_names[predictionKnn])  )
+print("LogReg = " +  str(iris.target_names[predictionLogReg]) + "\n")
 
-# --------------------------------------------------
+#excute model for calc accuracy
+accurKnn = PredictionAccuracy(iris, Model.KNN, TEST_SIZE)
+accurLogReg = PredictionAccuracy(iris, Model.KNN, TEST_SIZE)
+
+#print accuracy results
+print("Results for accuracy :")
+print("Knn = " + str(accurKnn) )
+print("LogReg = " + str(accurLogReg) + "\n")
+
+#--------------------------------------------------
